18294ce880653e3d55ae2983a271e4b72db2a311
sakhiasyam18 <asyamsakhimaulana@gmail.com>
2024-09-26 21:36:43 +0700

    gabutt

---
+++ belajar-for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int a = 1; a <= 10; a++){
+        cout<<"kuadrat dari "<<a <<" adalah " << a*a<<endl;
+
+    }
+
+    return 0;
+}
