95cb704ffb4a41466c496cfc778d85750fe6da4c
sakhiasyam18 <asyamsakhimaulana@gmail.com>
2025-03-06 09:30:28 +0700

    php-matkul

---
+++ php/tipedata.php
@@ -0,0 +1,17 @@
+<?php
+
+$teks = "hello word";
+$desimal = 10.5;
+$boolean = true;
+$buah = array("apel", "mangga", "pisang");
+$buah = ["apel", "mangga", "pisang"];
+$nilai = null;
+echo $desimal;
+
+echo $nilai;
+$b = 3;
+$a = 10;
+echo "\n";
+echo $a;
+echo "\n";
+echo $b;
